2c59cbbdf20909174482e1d69d76a4cc60b651bb
Johnatan Dev <lenshubperu@gmail.com>
2026-05-12 22:22:14 -0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Navbar from "@/components/Navbar";
 import Hero from "@/sections/Hero";
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-    </main>
-  );
+  return <Hero />;
 }
